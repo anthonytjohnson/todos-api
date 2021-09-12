b841de62ca90e49c48c1e3fdb8d7e217c6aae1fc
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-09-12 21:17:00 +0900

    added tests for POST /todos/id

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -76,6 +76,25 @@ RSpec.describe 'Todos API', type: :request do
         expect(response.body).to match(/Validation failed: Created by can't be blank/)
       end
     end
+
+    #test suite for PUT /todos/:id
+    describe 'PUT /todos/:id' do
+      let(:valid_attributes) { { title: 'Shopping' } }
+
+      context 'when the record exists' do
+        before { put "/todos/#{todo_id}", params: valid_attributes }
+
+        it 'updates the record' do
+          expect(response.body).to be_empty
+        end
+
+        it 'returns status code 204' do
+          expect(response).to have_http_status(204)
+        end
+      end
+    end
+
+    
   endS
 
 
